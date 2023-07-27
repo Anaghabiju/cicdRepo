635b22d6b00efb40dceb6bfc018ee1354b90212a
Anagha Biju(UST <248862@ust.com>
2023-07-27 14:20:00 +0530

    completed

---
+++ src/test/java/com/ust/app/hellospring/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.app.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
